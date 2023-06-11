c3a9827041536784e29510d886d43c1644592662
Ahmet Ceylan <dev.ahmetceylan@gmail.com>
2023-06-11 07:37:58 +0300

    textFieldInput widget & Login Screen UI (mobile)

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:instagram_clone_flutter/google_api.dart';
 import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_clone_flutter/screens/login_screen.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
 
 void main() async {
@@ -36,9 +37,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
-          webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout()),
+      home: LoginScreen(),
+      // home: const ResponsiveLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout()),
     );
   }
 }
